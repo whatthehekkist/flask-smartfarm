e5d52ab595bbcb000bf1f6c65365927eb7405920
whatthehekkist <kyu-ing@naver.com>
2025-01-30 16:46:06 +0900

    v2.2: render deploy test

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
+# not in use. Start Command is used in render website
+
 #!/bin/bash
-gunicorn app:app
-#gunicorn app:app --bind 0.0.0.0:10000
+#gunicorn server:server
+#gunicorn server:server --bind 0.0.0.0:10000
+
+
